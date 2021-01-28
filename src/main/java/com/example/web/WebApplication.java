59ed903b24d75d64bed4d31fce510d9619f87afc
qianxiaoone <306093226@qq.com>
2021-01-28 15:27:50 +0800

    test

--- src/main/java/com/example/web/WebApplication.java
+++ src/main/java/com/example/web/WebApplication.java
@@ -3,6 +3,11 @@ package com.example.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+/**
+ * @author wang-hc
+ */
 @SpringBootApplication
 public class WebApplication {
 
